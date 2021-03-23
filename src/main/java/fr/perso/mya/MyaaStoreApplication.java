21d0612e116d5386f100e99a0688261fb92514a5
Mya <canabady.ramya@gmail.com>
2021-03-23 13:40:18 +0100

    second commit mvn proj

---
+++ src/main/java/fr/perso/mya/MyaaStoreApplication.java
@@ -0,0 +1,13 @@
+package fr.perso.mya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyaaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyaaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/perso/mya/MyaaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.perso.mya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyaaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
